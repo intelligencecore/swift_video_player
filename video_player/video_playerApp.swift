3fd6e9d358ba2769f591e81940636e763aff5c77
Richie <richie@Richies-MacBook-Pro.local>
2025-04-04 19:59:48 -0400

    Swift Video Player App

---
+++ video_player/video_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_playerApp.swift
+//  video_player
+//
+//  Created by Richie on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct video_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ video_playerTests/video_playerTests.swift
@@ -0,0 +1,16 @@
+//
+//  video_playerTests.swift
+//  video_playerTests
+//
+//  Created by Richie on 4/3/25.
+//
+
+import Testing
+
+struct video_playerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
